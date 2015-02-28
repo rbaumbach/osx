ba8278408be223d9bedcb35f09258498037ab880
rbaumbach <rbaumbach.github@gmail.com>
2015-02-28 07:46:33 -0800

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'osx'
-maintainer       'Pivotal Labs'
-maintainer_email 'accounts@pivotallabs.com'
+maintainer       'Ryan Baumbach'
+maintainer_email 'rbaumbach.github@gmail.com'
 license          'MIT'
-description      'A collection of useful LWRPs for managing OSX'
+description      'A fork of "Pivotal-Sprout" osx, A collection of useful LWRPs for managing OSX'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 supports         'mac_os_x'
